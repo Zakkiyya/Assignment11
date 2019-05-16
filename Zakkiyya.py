989c0672965897f389fa6a132fea79d623c1f5b7
Zakkiyya <47412878+Zakkiyya@users.noreply.github.com>
2019-05-16 13:48:36 +0200

    Add files via upload

---
+++ Zakkiyya.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mpl
+
+data = pd.read_csv('tips.csv')
+print(data)
+data['paid'] = data.tip + data.total_bill
+paid = data.paid
+print(data)
+male = data[data.sex == 'Male']
+female = data[data.sex == 'Female']
+print(male)
+print(female)
+
+cor = data["total_bill"].corr(data["tip"])
+print(cor)
+mpl.bar("Male", np.mean(male.tip))
+mpl.bar("Female", np.mean(female.tip))
+mpl.ylabel("Tips")
+mpl.xlabel("Sex")
+mpl.title("Male vs Female Tippers")
+mpl.show()
